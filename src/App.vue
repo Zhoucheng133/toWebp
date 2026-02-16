f9d0d33f2af574ea0a605ca1462477efd3a619f8
zhoucheng <zhoucheng133@outlook.com>
2026-02-16 21:44:25 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  Hello
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import { getCurrentWindow } from '@tauri-apps/api/window';
+onMounted(async ()=>{
+  const appWindow = getCurrentWindow()
+  appWindow.show();
+})
+
+</script>
