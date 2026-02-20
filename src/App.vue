d6cb3bc119b9440a44ea346aaecaeaf6cb011356
zhoucheng <zhoucheng133@outlook.com>
2026-02-20 22:59:50 +0800

    添加初始界面

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
 <template>
-  <v-btn>按钮</v-btn>
+  <AddView v-if="path==null" />
 </template>
 
 <script setup lang="ts">
 import { onMounted } from 'vue';
 import { getCurrentWindow } from '@tauri-apps/api/window';
+import { storeToRefs } from 'pinia';
+import store from './store';
+import AddView from './views/AddView.vue';
+import { useTheme } from 'vuetify';
+const theme = useTheme();
+
 onMounted(async ()=>{
   const appWindow = getCurrentWindow()
   appWindow.show();
+  const systemTheme = await appWindow.theme();
+  theme.change(systemTheme || 'light');
+  await appWindow.listen('tauri://theme-changed', (event) => {
+    theme.change(event.payload as string)
+  })
 })
+
+let { path }=storeToRefs(store());
 
 </script>
---
+++ src/views/AddView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="page">
+    <v-btn icon="mdi-plus" variant="plain"></v-btn>
+    <div class="label">添加一个图片文件，或者拖拽到这里</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.page{
+  height: 100vh;
+  width: 100vw;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  box-sizing: border-box;
+  padding-bottom: 20px;
+  user-select: none;
+  -webkit-user-select: none;
+  gap: 5px;
+  font-size: 14px;
+}
+
+@media (prefers-color-scheme: dark) {
+  .page{
+    background-color: rgb(50, 50, 50);
+  }
+}
+</style>
