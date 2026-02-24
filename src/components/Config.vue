b5d9c8f7df021d573a44234b89cbfd0911ea9016
zhoucheng <zhoucheng133@outlook.com>
2026-02-24 22:33:21 +0800

    添加配置页

---
+++ src/components/Config.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    
+  </div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped>
+.container{
+  height: 100%;
+  width: 100%;
+  border-radius: 10px;
+  background-color: white;
+}
+
+@media (prefers-color-scheme: dark) {
+  .container{
+    background-color: black;
+  }
+}
+</style>
