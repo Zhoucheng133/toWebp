34daee0b7235da1058e8f154cdc91e3ad7df9014
zhoucheng <zhoucheng133@outlook.com>
2026-02-25 21:31:21 +0800

    添加菜单

--- src/components/Config.vue
+++ src/components/Config.vue
@@ -1,22 +1,64 @@
 <template>
   <div class="container">
-    
+    <ConfigMenu />
+    <div class="config_content">
+      <div class="title">{{ title }}</div>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import ConfigMenu from './ConfigMenu.vue';
+import { storeToRefs } from 'pinia';
+import store from '../store';
+import { computed } from 'vue';
+
+const { files, selectedIndex } = storeToRefs(store());
+
+const title = computed(() => {
+  if(files.value!=null && selectedIndex.value!=null && files.value.length>0 && selectedIndex.value<files.value.length){
+    return files.value[selectedIndex.value].name;
+  }
+  return "";
+});
+
 </script>
 
 <style scoped>
+.config_content{
+  flex: 1;
+  overflow: auto;
+  border-radius: 10px;
+  background-color: white;
+  padding: 15px;
+  margin-top: 10px;
+}
+.title{
+  flex: 1;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  font-size: 20px;
+}
+.title_bar{
+  display: flex;
+  width: 100%;
+  align-items: center;
+}
 .container{
   height: 100%;
   width: 100%;
-  border-radius: 10px;
-  background-color: white;
+  box-sizing: border-box;
+  user-select: none;
+  -moz-user-select: none;
+  -webkit-user-select: none;
+  display: flex;
+  min-height: 0;
+  flex-direction: column;
 }
 
 @media (prefers-color-scheme: dark) {
-  .container{
+  .config_content{
     background-color: black;
   }
 }
---
+++ src/components/ConfigMenu.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="menu">
+    <v-btn variant="flat" color="primary" prepend-icon="mdi-play">转换</v-btn>
+    <v-btn variant="text" prepend-icon="mdi-cogs">应用到所有</v-btn>
+    <v-btn variant="text" prepend-icon="mdi-close" style="margin-left: auto;" @click="close">关闭</v-btn>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { storeToRefs } from 'pinia';
+import store from '../store';
+
+const { files, selectedIndex } = storeToRefs(store());
+
+function close(){
+  files.value=null;
+  selectedIndex.value = null;
+}
+</script>
+
+<style scoped>
+.menu{
+  display: flex;
+  gap: 10px;
+}
+</style>
