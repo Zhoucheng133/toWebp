3be1918750ffd8fcf1f5c5587ae9b61b6774631c
zhoucheng <zhoucheng133@outlook.com>
2026-02-25 22:24:13 +0800

    完成配置界面

--- src/components/Config.vue
+++ src/components/Config.vue
@@ -3,6 +3,60 @@
     <ConfigMenu />
     <div class="config_content">
       <div class="title">{{ title }}</div>
+      <div class="config_item mt-4">
+        <div class="config_item_label">路径</div>
+        <div class="config_item_child line-clamp-2">{{ files![selectedIndex!].path }}</div>
+      </div>
+      <div class="config_item mt-2">
+        <div class="config_item_label">质量</div>
+        <div class="config_item_child flex" style="padding-right: 20px; box-sizing: border-box; gap: 15px;">
+          <v-slider v-model="files![selectedIndex!].quality" :step="10" :min="0" :max="100" style="flex: 1;" hide-details></v-slider>
+          <v-text-field
+            v-model="files![selectedIndex!].quality"
+            density="compact"
+            type="number"
+            style="width: 90px; max-width: 90px;"
+            variant="outlined"
+            hide-details
+          ></v-text-field>
+        </div>
+      </div>
+      <div class="config_item mt-2">
+        <div class="config_item_label">尺寸</div>
+        <div class="config_item_child flex" style="gap: 15px; align-items: center;">
+          <v-text-field
+            v-model="files![selectedIndex!].width"
+            density="compact"
+            type="number"
+            style="width: 120px; max-width: 150px;"
+            variant="outlined"
+            hide-details
+          ></v-text-field>
+          <v-icon icon="mdi-close"></v-icon>
+          <v-text-field
+            v-model="files![selectedIndex!].width"
+            density="compact"
+            type="number"
+            style="width: 120px; max-width: 150px;"
+            variant="outlined"
+            hide-details
+          ></v-text-field>
+        </div>
+      </div>
+      <div class="config_item mt-2">
+        <div class="config_item_label"></div>
+        <div class="tip flex align-center gap-1">
+          <i class="fa-solid fa-circle-info"></i>
+          <p>0表示自动宽/高度，都为0则原始尺寸</p>
+        </div>
+      </div>
+    </div>
+    <div class="config_item" style="margin-top: 10px;">
+      <div class="config_item_label">输出</div>
+      <div class="config_item_child flex gap-1">
+        <v-text-field v-model="output" density="compact" variant="outlined" hide-details></v-text-field>
+        <v-btn variant="flat">选择</v-btn>
+      </div>
     </div>
   </div>
 </template>
@@ -13,7 +67,7 @@ import { storeToRefs } from 'pinia';
 import store from '../store';
 import { computed } from 'vue';
 
-const { files, selectedIndex } = storeToRefs(store());
+const { files, selectedIndex, output } = storeToRefs(store());
 
 const title = computed(() => {
   if(files.value!=null && selectedIndex.value!=null && files.value.length>0 && selectedIndex.value<files.value.length){
@@ -25,6 +79,19 @@ const title = computed(() => {
 </script>
 
 <style scoped>
+.config_item_child{
+  min-width: 0;
+  text-align: left;
+  width: 100%;
+}
+.config_item_label{
+  width: 70px;
+  flex-shrink: 0;
+}
+.config_item{
+  display: flex;
+  align-items: center;
+}
 .config_content{
   flex: 1;
   overflow: auto;
@@ -32,9 +99,10 @@ const title = computed(() => {
   background-color: white;
   padding: 15px;
   margin-top: 10px;
+  display: flex;
+  flex-direction: column;
 }
 .title{
-  flex: 1;
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
--- src/store/index.ts
+++ src/store/index.ts
@@ -9,6 +9,9 @@ enum Status{
 
 export class TaskItem{
   public status: Status=Status.wait;
+  public quality: number=80;
+  public height: number=0;
+  public width: number=0;
   constructor(
     public path: string,
     public name: string,
@@ -21,8 +24,11 @@ export default defineStore("store", ()=>{
 
   const selectedIndex=ref<null | number>(null);
 
+  const output=ref("");
+
   return {
     files,
-    selectedIndex
+    selectedIndex,
+    output
   }
 })
